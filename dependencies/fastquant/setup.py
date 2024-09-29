4f8656c669d14614bf3429a4d2aa779b152b14bc
aLca <6356217+itsXactlY@users.noreply.github.com>
2024-09-30 01:48:32 +0200

    Update setup.py

--- dependencies/fastquant/setup.py
+++ dependencies/fastquant/setup.py
@@ -1,9 +1,15 @@
 import setuptools
+import os
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-with open("python/requirements.txt", "r") as fh:
+# Read the requirements from the requirements.txt file
+with open("requirements.txt", "r") as fh:
     install_requires = fh.read().splitlines()
+
+# Ensure README.md is read correctly for long description, if available
+long_description = ""
+if os.path.exists("README.md"):
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
 
 setuptools.setup(
     name="fastquant",
@@ -14,15 +20,19 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/enzoampil/fastquant",
-    packages=setuptools.find_packages(where="./python", exclude=["docs", "tests"]),
+    packages=setuptools.find_packages(where="python", exclude=["docs", "tests"]),
     package_dir={"": "python"},
     package_data={"fastquant": ["data/*"]},
     include_package_data=True,
-    scripts=["python/scripts/get_disclosures", "python/scripts/update_cache"],
+    scripts=[
+        os.path.join("python", "scripts", "get_disclosures"),
+        os.path.join("python", "scripts", "update_cache")
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     install_requires=install_requires,
+    python_requires='>=3.6',  # Specify Python version requirement
 )
